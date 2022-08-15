9b9a56f0b7149aaaeaaae2a33b73ae7f35fbb4e7
Kal <kal.sibbel@gmail.com>
2022-08-15 11:35:48 -0700

    Added a Custom Item and Custom Block

--- src/main/java/net/kal/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/kal/tutorialmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.kal.tutorialmod.block;
 
 import net.kal.tutorialmod.TutorialMod;
+import net.kal.tutorialmod.block.custom.JumpyBlock;
 import net.kal.tutorialmod.item.ModCreativeModeTab;
 import net.kal.tutorialmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -35,6 +36,10 @@ public class ModBlocks {
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3,7)), ModCreativeModeTab.TUTORIAL_TAB);
+
+    public static final RegistryObject<Block> JUMPY_BLOCK = registerBlock("jumpy_block",
+            () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TUTORIAL_TAB);
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
